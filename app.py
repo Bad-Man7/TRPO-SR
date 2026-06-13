2663921652639e193e04d7efd397eab996f76bf0
Bad-Man7 <almirmingazov32@gmail.com>
2026-06-13 12:45:22 +0000

    Подключение шаблона к главной странице

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Маршрут для главной страницы
 @app.route('/')
 def index():
-    return "<h1>Заметки с тегами</h1><p>Прототип запущен</p>"
+    return render_template('index.html')
 
 # Маршрут для добавления заметки
 @app.route('/add', methods=['GET', 'POST'])
